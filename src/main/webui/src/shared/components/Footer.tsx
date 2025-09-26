f7356cd75a602742a700cc636447e834d050d576
Stefan Poindl <stefan@tohuwabohu.io>
2025-09-26 21:22:54 +0200

    fix admin ui version display

--- src/main/webui/src/shared/components/Footer.tsx
+++ src/main/webui/src/shared/components/Footer.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import { getAppVersion } from '../utils/version'
 
 const Footer: React.FC = () => {
+  const appVersion = getAppVersion()
+
   return (
     <div className="text-center mt-8">
       <div className="text-tui-muted text-xs">
         <p>&copy; {new Date().getFullYear()} tohuwabohu.io</p>
-        <p className="mt-1"><a href="https://github.com/thwbh/crt-dojo" target="_blank">crt-dojo</a> • admin ui v{getAppVersion()}</p>
+        <p className="mt-1"><a href="https://github.com/thwbh/crt-dojo" target="_blank">crt-dojo</a> • admin
+          {appVersion !== 'unknown' && ( <a target="_blank" href={`https://github.com/thwbh/kamifusen/releases/tag/${appVersion}`}> v{appVersion}</a>)}
+        </p>
       </div>
     </div>
   )
